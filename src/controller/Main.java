e3c24cc4aa5d0fdac56e656596d52395a5fa14bc
ChaoAlarcón <chaoalarcon03@gmail.com>
2025-04-04 10:54:27 +0200

    .

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,16 +1,13 @@
 package controller;
+
 import model.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Estudiante estudiante = new Estudiante();
-		System.out.println("¡Bienvenido a Fire Emblem Three Houses!");
-		estudiante.elegirEstudiante();
-		Combate combate = new Combate();
-		combate.iniciarCombate();
 		
-		
+		Usuario.bienvenida();
+
 	}
 
 }
---
+++ src/model/Usuario.java
@@ -0,0 +1,69 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Usuario {
+
+	public static void bienvenida() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Bienvenido a Fire Emblem Three Houses");
+		System.out.println("¿Cuál es tu nombre?:");
+		String nombreUsuario = sc.nextLine();
+		System.out.println("Seas bienvenido, " + nombreUsuario + ".");
+		crearListaEnemigos();
+	}
+
+	public static void pulsarEnter() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Pulsa ENTER para continuar");
+		sc.nextLine();
+	}
+	
+	public static void elegirPersonaje(ArrayList<Enemigo> enemigos) {
+		// ELIGIENDO ESTUDIANTE
+				int vidaEstudiante = (int) (Math.random() * (20 + 1)) + 40;
+				int ataqueEstudiante = (int) (Math.random() * (5 + 1)) + 7;
+				int defensaEstudiante = (int) (Math.random() * (3 + 1)) + 4;
+				int vidaMaxEstudiante = vidaEstudiante;
+
+				int nivel = 1;
+				int estudianteElegido = Estudiante.estudianteElegido();
+				Estudiante seleccionado;
+				if (estudianteElegido == 1) {
+					System.out.println("Has elegido a:");
+					Estudiante hubert = new Estudiante("Hubert", Casa.AGUILAS_NEGRAS, vidaEstudiante, ataqueEstudiante,
+							defensaEstudiante, nivel);
+					hubert.imprimirInfoParcial();
+					seleccionado = hubert;
+				} else if (estudianteElegido == 2) {
+					System.out.println("Has elegido a:");
+					Estudiante dimitri = new Estudiante("Dimitri", Casa.LEONES_AZULES, vidaEstudiante, ataqueEstudiante,
+							defensaEstudiante, nivel);
+					dimitri.imprimirInfoParcial();
+					seleccionado = dimitri;
+				} else {
+					System.out.println("Has elegido a:");
+					Estudiante claude = new Estudiante("Claude", Casa.CIERVOS_DORADOS, vidaEstudiante, ataqueEstudiante,
+							defensaEstudiante, nivel);
+					claude.imprimirInfoParcial();
+					seleccionado = claude;
+				}
+				// FIN ELEGIENDO ESTUDIANTE
+				Enemigo.numeroDeEnemigos(enemigos);
+				Combate.combate(enemigos, seleccionado, vidaEstudiante, ataqueEstudiante, defensaEstudiante, vidaMaxEstudiante);
+				Estudiante.resultadoFinal(seleccionado);
+	}
+	
+	public static void crearListaEnemigos() {
+		ArrayList<Enemigo> enemigos = new ArrayList<Enemigo>();
+		enemigos.add(new Enemigo("Seteth", 30, 8, 2, Destreza.DEBIL));
+		enemigos.add(new Enemigo("Flayn", 50, 10, 4, Destreza.FUERTE));
+		enemigos.add(new Enemigo("Manuela", 30, 8, 2, Destreza.DEBIL));
+		enemigos.add(new Enemigo("Gilbert", 50, 10, 4, Destreza.FUERTE));
+		enemigos.add(new Enemigo("Alois", 30, 8, 2, Destreza.DEBIL));
+
+		elegirPersonaje(enemigos);
+	}
+
+}
